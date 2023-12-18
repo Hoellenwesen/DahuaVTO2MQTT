cb19d6dfd30c643ac9f6607fedff73326386e45e
Hoellenwesen <sebastian@weyer.link>
2023-12-18 16:40:16 +0100

    removed regex, work with json index

--- common/consts.py
+++ common/consts.py
@@ -52,4 +52,4 @@ TOPIC_COMMAND = "/Command"
 TOPIC_DOOR = "Open"
 TOPIC_MUTE = "Mute"
 
-MESSAGE_PREFIX_PATTERN = "DHIP[a-zA-Z^!@#%&*()\\]\\[_+-=;':""\",./<>?~`\\n]{0,2}[a-zA-Z^!@#%&*()\\]\\[_+-=;':""\",./<>?~`\\n]{0,2}"
+JSON_START_PATTERN = "{\""
